b6e98295b9c9eb670f303a2c900a6085fe35db3e
caetanocardoso <120603190+caetanocardoso@users.noreply.github.com>
2023-11-14 19:34:39 -0300

    Update main.js

--- model126project-main/main.js
+++ model126project-main/main.js
@@ -3,7 +3,7 @@ function setup() {
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
-    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/zozsgvdln/',modelLoaded);
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/zozsgvdln/model.json',modelLoaded);
   }
   
   function modelLoaded() {
